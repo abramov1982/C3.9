b8a6a28692922816064da42a08d2b5d7e14113f2
abramov1982 <49936583+abramov1982@users.noreply.github.com>
2020-02-14 15:52:14 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -14,22 +14,29 @@ city_submit.onclick = () => {
     alert(city_input.value)
 };
 
-submit_point.onclick = () => {
-    if (one.checked) alert('got it')
-    if (two.checked) alert('got it')
-    if (three.checked) alert('got it')
-    if (four.checked) alert('got it')
-    if (five.checked) alert('got it')
-    if (six.checked) alert('got it')
-  
 function getCookie(name) {
   let matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
+  ));
   return matches ? decodeURIComponent(matches[1]) : undefined;
-};
+}
 
-function setCookie(name, value) {
-  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-  document.cookie = updatedCookie;
-};
+//function setCookie(name, value) {
+//  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+//  document.cookie = updatedCookie;
+//};
+
+submit_point.onclick = () => {
+  if (one.checked) console.log('got it')
+  document.cookie = 'one=True'
+  if (two.checked) console.log('got it')
+  setCookie(two,'True')
+  if (three.checked) console.log('got it')
+  setCookie(three,'True')
+  if (four.checked) console.log('got it')
+  setCookie(four,'True')
+  if (five.checked) console.log('got it')
+  setCookie(five,'True')
+  if (six.checked) console.log('got it')
+  setCookie(six,'True')
+}
